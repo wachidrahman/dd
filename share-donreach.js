ded00db314b6c6fda45c318d37335a96acb515fa
wachidrahman <wachidrahman@gmail.com>
2016-08-14 15:37:11 +0700

    Add files via upload

---
+++ share-donreach.js
@@ -0,0 +1,6 @@
+  (function() {
+    var dr = document.createElement('script');
+    dr.type = 'text/javascript'; dr.async = true;
+    dr.src = '//cdn.rawgit.com/wachidrahman/dd/master/buttons.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dr);
+  })();
